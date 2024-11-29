26bd8b9ae5a897e93b4e1b31b33991f302a3ed7b
weixuefeng <weixuefeng@diynova.com>
2024-11-29 21:45:31 +0800

    feat: add decode bitcoin tx

--- src/pages/api/tx.ts
+++ src/pages/api/tx.ts
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ErrorCode } from 'constant'
+import { BitcoinNetworkType, ChainConfig, ErrorCode } from 'constant'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Transaction } from 'ethers'
+import { hexlify, Transaction } from 'ethers'
+import * as bitcoin from 'bitcoinjs-lib'
+import * as ecc from 'tiny-secp256k1'
+bitcoin.initEccLib(ecc)
 
 type ResponseData = {
   errorCode: number
@@ -14,10 +17,33 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Respon
   const chain = content['chain']
   const tx = content['tx']
   try {
-    var response = parseEvmTx(tx)
+    var response = parseTransaction(chain, tx)
     jsonSuccessResponse(response, res)
   } catch (e) {
     jsonErrorResponse(e.message, res)
+  }
+}
+
+function jsonSuccessResponse(object: object, res: NextApiResponse<ResponseData>) {
+  res.status(200).json({ errorCode: ErrorCode.Success, result: object, errorMsg: '' })
+}
+
+function jsonErrorResponse(errorMsg: string, res: NextApiResponse<ResponseData>) {
+  res.status(200).json({ errorCode: ErrorCode.Error, result: {}, errorMsg: errorMsg })
+}
+
+function parseTransaction(chain: string, tx: string): object {
+  switch (chain) {
+    case ChainConfig.Bitcoin:
+      return parseBitcoinTransaction(tx, BitcoinNetworkType.MainNet)
+    case ChainConfig.BitcoinRegTest:
+      return parseBitcoinTransaction(tx, BitcoinNetworkType.RegTest)
+    case ChainConfig.BitcoinTestNet:
+      return parseBitcoinTransaction(tx, BitcoinNetworkType.TestNet)
+    case ChainConfig.ETH:
+      return parseEvmTx(tx)
+    default:
+      throw Error(`unknow chain type: ${chain}`)
   }
 }
 
@@ -45,10 +71,56 @@ function parseEvmTx(txString: string) {
   }
 }
 
-function jsonSuccessResponse(object: object, res: NextApiResponse<ResponseData>) {
-  res.status(200).json({ errorCode: ErrorCode.Success, result: object, errorMsg: '' })
+function getBitcoinNetworkByFlag(networkFlag: number): bitcoin.Network {
+  switch (networkFlag) {
+    case BitcoinNetworkType.MainNet:
+      return bitcoin.networks.bitcoin
+    case BitcoinNetworkType.TestNet:
+      return bitcoin.networks.testnet
+    case BitcoinNetworkType.RegTest:
+      return bitcoin.networks.regtest
+    default:
+      throw Error(`unknow networkFlag ${networkFlag}`)
+  }
 }
 
-function jsonErrorResponse(errorMsg: string, res: NextApiResponse<ResponseData>) {
-  res.status(200).json({ errorCode: ErrorCode.Error, result: {}, errorMsg: errorMsg })
+function parseBitcoinTransaction(txString: string, networkFlag: number) {
+  console.log(`network ${networkFlag}`)
+  var network = getBitcoinNetworkByFlag(networkFlag)
+  var resRaw = bitcoin.Transaction.fromHex(txString)
+  var inputs = []
+  resRaw.ins.forEach((item, index) => {
+    var input = {} as any
+    input.hash = hexlify(item.hash.reverse())
+    input.index = item.index
+    input.sequence = item.sequence
+    input.witness = []
+    input.script = hexlify(item.script)
+    item.witness.forEach((w, i) => {
+      input.witness.push(hexlify(w))
+    })
+    inputs.push(input)
+  })
+  var outputs = []
+  resRaw.outs.forEach((item, index) => {
+    console.log(hexlify(item.script))
+    var output = {} as any
+    output.value = item.value.toString()
+    output.script = hexlify(item.script)
+    output.address = bitcoin.address.fromOutputScript(item.script, network)
+    outputs.push(output)
+  })
+  var result = {
+    hash: resRaw.getId(),
+    version: resRaw.version,
+    inputs: inputs,
+    outputs: outputs,
+    locktime: resRaw.locktime,
+    virtualSize: resRaw.virtualSize(),
+    vin_size: resRaw.ins.length,
+    vout_size: resRaw.outs.length,
+    size: resRaw.byteLength(),
+    total_output: resRaw.outs.reduce((prev, item) => prev + parseInt(item.value.toString()), 0),
+  }
+  return result
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import styles from 'styles/Home.module.scss'
 import { SkipLink } from 'components/Links'
 import { Header } from 'components/Header/Header'
 import { Footer } from 'components/Footer'
-import { Button, Description, Field, Label, Textarea } from '@headlessui/react'
+import { Button, Description, Field, Label, Textarea, Select } from '@headlessui/react'
 import clsx from 'clsx'
 import { submitRawTx } from 'network'
 import { useState } from 'react'
+import { SupportChains } from 'constant'
 
 export default function Home() {
   return (
@@ -33,14 +34,19 @@ function Main() {
 function ActionArea() {
   const [rawTx, setRawTx] = useState<string>()
   const [parsedTx, setPrasedTx] = useState<object>()
+  const [currentChain, setCurrentChain] = useState<string>(SupportChains[0])
 
   const handleInputChange = event => {
     console.log(event.target.value)
     setRawTx(event.target.value)
   }
 
+  const handleSelectChain = event => {
+    setCurrentChain(event.target.value)
+  }
+
   const handleSubmitTx = () => {
-    submitRawTx('eth', rawTx)
+    submitRawTx(currentChain, rawTx)
       .then(res => {
         setPrasedTx(res)
       })
@@ -54,18 +60,29 @@ function ActionArea() {
       <Field>
         <Label className="text-sm/6 font-medium text-black">Description</Label>
         <Description className="text-sm/6 text-black/50">This will be shown under the product title.</Description>
+
+        <Field>
+          <Label className="block">SelectChain</Label>
+          <Select onChange={handleSelectChain} className="mt-1 block" name="country">
+            {SupportChains &&
+              SupportChains.map(item => {
+                return <option key={item}>{item}</option>
+              })}
+          </Select>
+        </Field>
+
         <Textarea
           onInput={handleInputChange}
           className={clsx(
-            'mt-3 block w-full resize-none rounded-lg border-2 border-black bg-white/5 py-1.5 px-3 text-xl text-black ',
-            'data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 focus:outline-none'
+            'mt-3 block w-full resize-none rounded-lg border-2 border-black bg-white/5 px-3 py-1.5 text-xl text-black ',
+            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
           )}
           rows={3}
         />
       </Field>
       <Button
         onClick={handleSubmitTx}
-        className="ounded data-[hover]:bg-sky-500 data-[active]:bg-sky-700 mt-5 bg-sky-600 py-2 px-4 text-sm text-white"
+        className="ounded mt-5 bg-sky-600 px-4 py-2 text-sm text-white data-[active]:bg-sky-700 data-[hover]:bg-sky-500"
       >
         submit
       </Button>
